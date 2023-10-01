98efd3bd23fcabff5b2f160523918df3b95a48b5
Abraham Kong <abraham.kong@sjsu.edu>
2023-09-30 18:00:40 -0700

    upload part b of the assignment, waiting for the youtube video link

--- README.md
+++ README.md
@@ -3,11 +3,22 @@
 # Part A 
 ## Medium Article about Prompt Engineer Techniques
 
+Medium article:
 [Top 15 Prompt Engineering Techniques You Have to Know](https://medium.com/@abraham.jkong/top-15-prompt-engineering-techniques-you-have-to-know-53531c43fa32)
 
 
 [Prompts gathered for above article]()
 
 # Part B
+
+Use the help of Chat GPT 4 to write a streamlit web application 
+
+that makes user's input prompt better
+
+Medium article
+[Building Your AI-Enhanced Prompt Interface: A Step-by-Step Guide to Streamlit and GPT-4 Integration](https://medium.com/@abraham.jkong/building-your-ai-enhanced-prompt-interface-a-step-by-step-guide-to-streamlit-and-gpt-4-integration-cc739f7ad55c)
+
+
+[Youtube Video]()
 
 # Part C
---
+++ part b/app.py
@@ -0,0 +1,49 @@
+# Import necessary libraries
+import streamlit as st
+import openai
+
+# Configure OpenAI API key
+OPENAI_API_KEY = "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"
+openai.api_key = OPENAI_API_KEY
+
+
+def main():
+    st.title("World Class Prompt Beautifier")
+
+    desired_prompt = st.text_area("Enter your desired prompt:")
+
+    # Only display additional fields once a prompt is entered
+    if desired_prompt:
+        tone = st.selectbox("Select the desired tone (e.g., 'Friendly'):", [
+                            "Friendly", "Conversational", "Happy", "Formal", "Instructional", "Professional", "Sarcastic", "Inquisitive", "Inspiring"])
+        target_market = st.text_input(
+            "Enter the target market (if any, e.g., 'Millennials'):")
+        niche = st.text_input(
+            "Specify the niche (if any, e.g., 'Eco-friendly travel gear'):")
+        product_name = st.text_input(
+            "Enter the product name (if any, e.g., 'EcoTraveller Backpack'):")
+        famous_style = st.text_input(
+            "Optional: Enter a famous person's writing style you'd like to mimic (e.g., 'Ernest Hemingway'):")
+        other_info = st.text_area(
+            "Is there anything else you think might be helpful? (e.g., 'The product is made of 100% recycled materials.')")
+
+        # When user presses "Enhance Prompt", make a call to GPT-4 for an improved prompt
+        if st.button("Enhance Prompt"):
+            # Build the conversation messages
+            messages = [
+                {"role": "system", "content": "You are a world-class prompt engineer."},
+                {"role": "user", "content": f"Enhance this prompt based on the following parameters: Tone - {tone}, Target Market - {target_market}, Niche - {niche}, Product Name - {product_name}, Style - {famous_style}, Additional Information - {other_info}. The original prompt is: {desired_prompt}"}
+            ]
+
+            response = openai.ChatCompletion.create(
+                model="gpt-4",
+                messages=messages
+            )
+
+            # Display the enhanced prompt
+            enhanced_prompt = response['choices'][0]['message']['content']
+            st.text_area("Enhanced Prompt:", value=enhanced_prompt, height=200)
+
+
+if __name__ == "__main__":
+    main()
